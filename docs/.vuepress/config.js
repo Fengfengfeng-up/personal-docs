0bcc1bf6f168b371365cc86dfe331981df5949d0
冯风风 <1010423552@qq.com>
2020-12-26 16:40:54 +0800

    chore: update theme config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,38 +8,32 @@ module.exports = ctx => ({
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    [
-      'meta',
-      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-    ],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
     ['link', { rel: 'apple-touch-icon', href: `/apple-touch-icon.png` }],
   ],
   themeConfig: {
-    // repo: 'vuejs/vuepress',  // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    // // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    // repoLabel: '查看源码',
-    // docsRepo: 'vuejs/vuepress', // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsDir: 'docs', // 假如文档不是放在仓库的根目录下：
-
+    repo: 'Fengfengfeng-up/personal-docs',
+    repoLabel: 'GitHub',
+    docsBranch: 'main',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
     smoothScroll: true,
     // logo: '/logo.png',
     nav: [
-      { text: 'JavaScript', link: '../my-docs/js/' },
-      { text: 'TypeScript', link: '../my-docs/ts/' },
-      { text: 'CSS', link: '../my-docs/css/' },
-      { text: 'Vue', link: '../my-docs/vue/' },
-      { text: 'Webpack', link: '../my-docs/webpack/' },
-      { text: 'NestJs', link: '../my-docs/nestjs/' },
-      { text: '收藏', link: '../my-docs/collection/' },
+      { text: 'JavaScript', link: '/my-docs/js/' },
+      // { text: 'TypeScript', link: '/my-docs/ts/' },
+      // { text: 'CSS', link: '/my-docs/css/' },
+      // { text: 'Vue', link: '/my-docs/vue/' },
+      // { text: 'Webpack', link: '/my-docs/webpack/' },
+      // { text: 'NestJs', link: '/my-docs/nestjs/' },
+      { text: '收藏', link: '/my-docs/collection/' },
+      { text: '博客', link: 'https://www.striveforus.com/', target: '_blank' },
     ],
-    sidebar: 'auto',
-    // algolia: ctx.isProd
-    //   ? {
-    //       apiKey: '3a539aab83105f01761a137c61004d85',
-    //       indexName: 'vuepress',
-    //     }
-    //   : null,
+    sidebar: {
+      '/my-docs/js/': getJsSidebar('基础部分', '进阶部分')
+    },
+    lastUpdated: '上次更新',
   },
   plugins: [
     ['@vuepress/back-to-top', true],
@@ -71,6 +65,27 @@ module.exports = ctx => ({
     },
   },
   // extraWatchFiles: [
+  //   './.vuepress'
   // ],
   // evergreen: true, // 禁止 ESNext 到 ES5 的转译以及对 IE 的 polyfills，同时会带来更快的构建速度和更小的文件体积
 })
+
+function getJsSidebar (groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [
+        '',
+        'basic-types'
+      ]
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      children: [
+        'design-mode'
+      ]
+    }
+  ]
+}
