f0c26c020f8ca9283b73654f001155a5dffb7d1a
冯风风 <1010423552@qq.com>
2020-12-29 22:22:00 +0800

    feat: add flowchart plugin

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,7 +42,7 @@ module.exports = ctx => ({
     //   updatePopup: true
     // }],
     ['@vuepress/medium-zoom', true],
-    // ['flowchart']
+    ['flowchart']
   ],
   configureWebpack: {
     resolve: {
